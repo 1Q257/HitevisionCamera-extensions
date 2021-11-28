672d9f117daf778c1e4f215d6a81cc41792b8c6a
liubanlaobanzhang <3143786339@qq.com>
2021-11-28 14:02:14 +0800

    666

---
+++ 窗口置顶/tk-可拖动DEMO.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import Widget, messagebox
+import os
+print("鸿合展台第三方控件 | 版本 2021.11.20-22504")
+print("Powered By 老班长")
+print('https://gitee.com/liubanlaobanzhang/')
+path1="C:/Program Files (x86)/HiClass/HiteCamera/HiteCameraShell/HiteCameraShell.exe"
+tk2 = tk.Tk()
+def callback():
+    os.startfile(path1)
+img1=tk.PhotoImage(file='D:\LOGO\展台logo.png')
+button1 = tk.Button(tk2,image=img1,command=callback)
+button1.pack()
+
+
+class DragWindow:
+    def __init__(self):
+        tk2.x, tk2.y = 0, 0
+        self.window_size = '256x128'
+
+        # 设置隐藏窗口标题栏和任务栏图标
+        tk2.overrideredirect(True)
+        # 窗口透明度80%
+        tk2.attributes("-alpha", 0.2)
+        # 设置窗口大小、位置 长x宽+x+y
+        tk2.geometry(f"{self.window_size}+1700+900")
+        # 设定背景颜色
+        tk2.configure(bg="white")
+
+        # 窗口移动事件
+        tk2.bind("<B1-Motion>", self.move)
+        # 单击事件
+        tk2.bind("<Button-1>", self.get_point)
+        # 双击事件
+        tk2.bind("<Triple-Button-1>", self.close)
+
+    def move(self, event):
+        """窗口移动事件"""
+        new_x = (event.x - tk2.x) + tk2.winfo_x()
+        new_y = (event.y - tk2.y) + tk2.winfo_y()
+        s = f"{self.window_size}+{new_x}+{new_y}"
+        tk2.geometry(s)
+
+    def get_point(self, event):
+        """获取当前窗口位置并保存"""
+        tk2.x, tk2.y = event.x, event.y
+
+    def run(self):
+        tk2.mainloop()
+
+    def close(self, event):
+        tk2.destroy()
+
+
+if __name__ == "__main__":
+    window = DragWindow()
+    window.run()
+    
--- 窗口置顶/tk4.py
+++ 窗口置顶/tk4.py
@@ -5,15 +5,47 @@ import os
 print("鸿合展台第三方控件 | 版本 2021.11.20-22504")
 print("Powered By 老班长")
 print('https://gitee.com/liubanlaobanzhang/')
-tk2=tk.Tk()
-img1=tk.PhotoImage(file='D:\LOGO\展台logo.png')
-path1="C:/Users/Public/Desktop/鸿合视频展台.lnk"
-
-def callback():
-    os.startfile(path1)
+tk2=tk.Tk(className='666')
 
 
-button1 = tk.Button(tk2,image=img1,command=callback,text='站台',font='仿宋')
-button1.pack()
-tk2.geometry("128x128")
+
+
+class DragWindow:
+    def __init__(self):
+        # 窗口透明度80%
+        tk2.attributes("-alpha", 0.2)
+        tk2.x, tk2.y = 0, 0
+        self.window_size = '300x200'
+
+        tk2.overrideredirect(True)
+        tk2.attributes("-alpha", 0.4)
+
+        tk2.geometry(f"{self.window_size}+10+10")
+        # 设定背景颜色
+        tk2.configure(bg="blue")
+
+        # 窗口移动事件
+        tk2.bind("<B1-Motion>", self.move)
+
+        def move(self, event):
+            """窗口移动事件"""
+            new_x = (event.x - tk2.x) + tk2.winfo_x()
+            new_y = (event.y - tk2.y) + tk2.winfo_y()
+            s = f"{self.window_size}+{new_x}+{new_y}"
+            tk2.geometry(s)
+
+        def get_point(self, event):
+            """获取当前窗口位置并保存"""
+            tk2.x, tk2.y = event.x, event.y
+
+        def run(self):
+            tk2.mainloop()
+
+    def close(self, event):
+        tk2.destroy()
+
+window = DragWindow()
+window.run()
+
+tk2.geometry("256x128")
 tk2.mainloop()
